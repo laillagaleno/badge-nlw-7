0667a96b77d183ccb7629e496e7517ed65d1f977
Lailla Galeno <82004991+laillagaleno@users.noreply.github.com>
2021-11-20 21:08:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+const linksSocialMedia={
+    name:'Lailla Galeno',
+    github:'laillagaleno',
+    youtube: 'jakelinygracielly',
+    facebook:'lailla.galeno.9',
+    instagram:'lailla_ga',
+    twitter:''
+}
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class');
+        li.children[0].href= `https://${social}.com/${linksSocialMedia[social]}` 
+    }
+    //document.getElementById('userName').textContent = linksSocialMedia.name;
+    //userName.textContent = linksSocialMedia.name;
+}
+
+changeSocialMediaLinks();
+
+function getGitHybProfileInfos(){
+    const url =`https://api.github.com/users/${linksSocialMedia.github}`
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        userName.textContent = data.name;
+        userLogin.textContent = data.login;
+        userLike.href = data.html_url;
+        userBio.textContent = data.bio;
+        userImage.src=data.avatar_url;
+    })
+
+    //promise- pega respostas de alguma coisa
+}
+
+getGitHybProfileInfos();
+
+
+function toastNotificationShowMessage(msgText){
+    $("#msgToast").text(msgText);
+    $("#toast").addClass("show");
+    setTimeout(function(){
+        $("#toast").removeClass('show');
+    }, 3000);
+}
+
+document.getElementById('userName').addEventListener('click', copyToClipboard);
+async function copyToClipboard(){
+    let content = document.getElementById('userName').innerHTML;
+    await navigator.clipboard.writeText(content);
+    toastNotificationShowMessage("Copiado!");
+}
+
+        var youtubeLinkButton = document.getElementById('youtube');
+        var facebookLinkButton = document.getElementById('facebook');
+        var instagramLinkButton = document.getElementById('instagram');
+        var twitterLinkButton = document.getElementById('twitter');
+
+        // twitterLinkButton.addEventListener('click', function(){
+        //     if ($(".twitter a").getAttribute("href")= "https://twitter.com/" ){
+        //         toastNotificationShowMessage("naão");
+        //         alert("oi");
+        //     }
+           
+        // })
+
+       // colocar um dado no firebase
+       
